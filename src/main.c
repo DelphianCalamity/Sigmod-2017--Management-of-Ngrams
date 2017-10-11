561a11d253c10f8e158e4b5cd5d6cbd96bc2ba9f
stathisKara <sdi1200057@di.uoa.gr>
2017-10-11 13:56:58 +0300

    Simple main and memroy allocation for root

---
+++ src/main.c
@@ -0,0 +1,13 @@
+//
+// Created by Stathis on 10/11/2017.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "trieStructs.h"
+
+int main(void){
+    TrieRoot *trieRoot1;
+
+    trieRoot1=rootInit();
+}
---
+++ src/trieStructs.c
@@ -0,0 +1,20 @@
+//
+// Created by Stathis on 10/11/2017.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "trieStructs.h"
+
+TrieRoot * rootInit() {
+    TrieRoot *trieroot;
+
+    if ((trieroot = malloc(sizeof(TrieRoot))) == NULL) {
+//        perror(printError(1));
+        exit(1);
+    }
+    if ((trieroot->root = malloc(sizeof(TrieNode)))==NULL){
+//        perror(printError(1));
+        exit(1);
+    }
+}
+
