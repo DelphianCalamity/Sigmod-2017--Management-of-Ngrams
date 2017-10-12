e7279f628717c9cdfa230f36a47f7c61aa373a9f
stathisKara <sdi1200057@di.uoa.gr>
2017-10-12 23:02:12 +0300

    error messages

---
+++ src/errorMessages.c
@@ -0,0 +1,18 @@
+//
+// Created by Stathis on 10/12/2017.
+//
+
+char * getError(int code){
+    switch (code) {
+        case 1:  return("Error in memory allocation") ;
+
+        case 2:  return("Error in memory reallocation");
+
+        case 3:  return("");
+
+        case 4:  return("") ;
+
+        case 5:  return("") ;
+
+    }
+}
---
+++ src/errorMessages.h
@@ -0,0 +1,11 @@
+//
+// Created by Stathis on 10/12/2017.
+//
+
+#ifndef _ERRORMESSAGES_H
+#define _ERRORMESSAGES_H
+
+char * getError(int);
+
+
+#endif //_ERRORMESSAGES_H
