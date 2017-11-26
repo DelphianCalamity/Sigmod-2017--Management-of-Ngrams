1d1fc1590706a9b3fe644592e0e4695fbb033b8e
MaryLP <sdi1200095@di.uoa.gr>
2017-11-26 17:37:19 +0200

    bloom

---
+++ src/bloomFilter.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "bloomFilter.h"
+#include "errorMessages.h"
+#include "ngram.c"
+#include "trieStructs.h"
+
+bool findInBloom(NgramVector *ngram){
+	unsigned int i, place, bit, value;
+	char prev;
+	bool retval=true;
+
+	memset(cells, 0, K*sizeof(unsigned int));
+
+	for (i=0; i<K; i++){
+		//cells[i] = hash(ngram, i)%(BLOOMSIZE*8);
+		place = (cells[i]/8)%BLOOMSIZE;		/* place has the place of the byte that has the desired bit */
+		bit = cells[i]%8;					/* bit has the place of the desired bit in the byte */
+		prev = bloomFilter[place];			/* prev has the value of the corresponding byte in the bloom filter BEFORE the change */
+		value = 1;
+		value = value << bit;				/* creating the new value of the desired bit */
+		value = value | prev;				/* OR operation: value now has the new value of the byte that was to be changed */
+		if ((value^prev) == 0)				/* if the XOR operation returns a non-zero value, it means the bit was changed (we have not encountered this ngram before) */
+			retval = false;					/* if retval is false (at least one bit was changed), the ngram has not been encountered yet; print it */
+		bloomfilter[place] = value;		
+	}
+	return retval;
+}
---
+++ src/bloomFilter.h
@@ -0,0 +1,15 @@
+#ifndef __BLOOMFILTER_H
+#define __BLOOMFILTER_H
+
+#include <stdbool.h>
+#include "ngram.h"
+
+#define BLOOMSIZE 16384			// it's actually 8 times more, since we're playing with bits, not whole bytes
+#define K 10
+
+char *bloomFilter;
+unsigned int *cells;
+
+bool findInBloom(NgramVector*);
+
+#endif
